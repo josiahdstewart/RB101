d3fa2a292567903761e9228e23c6b398cd334f08
Josiah Stewart <josiahdstewart@gmail.com>
2024-02-26 02:23:08 -0600

    added scorekeeping ability

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -1,6 +1,9 @@
 # solicit user and computer input, compare, show winner
+require "pry"
+
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
 COMPARE_CHOICES = {rock: ['scissors', 'lizard'], paper: ['rock', 'spock'], scissors: ['paper', 'lizard'], spock: ['scissors', 'rock'], lizard: ['paper', 'spock']}
+WINNING_SCORE = 3
 
 def prompt(message)
   puts("=> #{message}")
@@ -31,31 +34,55 @@ def display_result(player, computer)
   end
 end
 
+def add_score(player, computer, score1, score2)
+end
+
+def evaluate_score(score1, score2)
+  if score1 == WINNING_SCORE
+    prompt "You are the Grand Champion!"
+  elsif score2 == WINNING_SCORE
+    prompt "Uh oh, you fell off the podium; the computer is the grand champion."
+  end
+end
+
+player_score = 0
+computer_score = 0
 loop do
   choice = ''
-  loop do
-    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
-    prompt "Type the first two letters of your choice: "
-    abr_choice = gets.chomp
-    
-    choice = abr_to_full(abr_choice)
+  while player_score < 3 && computer_score < 3 do
+    loop do
+      prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+      prompt "Type the first two letters of your choice: "
+      abr_choice = gets.chomp
 
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
+      choice = abr_to_full(abr_choice)
+
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt "You chose: #{choice}; computer chose: #{computer_choice}"
+
+    display_result(choice, computer_choice)
+
+    if win?(choice, computer_choice)
+      player_score += 1
+    elsif win?(computer_choice, choice)
+      computer_score += 1
+    end
+    evaluate_score(player_score, computer_score)
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  prompt "You chose: #{choice}; computer chose: #{computer_choice}"
-
-  display_result(choice, computer_choice)
 
   prompt "Do you want to play again?"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
+  player_score = 0
+  computer_score = 0
 end
 
 prompt "Thank you for playing! Good-bye."
